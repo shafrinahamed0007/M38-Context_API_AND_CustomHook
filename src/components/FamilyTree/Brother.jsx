00a01d28056bd263557dc2c2b6f398c3a0c83067
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-04-06 14:11:17 +0600

    Up to date'

--- src/components/FamilyTree/Brother.jsx
+++ src/components/FamilyTree/Brother.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { MoneyContext } from "./FamilyTree";
 
 const Brother = () => {
+  const [money, setMoney] = useContext(MoneyContext);
   return (
     <div>
       <h3>Brother</h3>
+      <button onClick={() => setMoney(money + 1000)}>Add 1000 TK</button>
     </div>
   );
 };
--- src/components/FamilyTree/FamilyTree.jsx
+++ src/components/FamilyTree/FamilyTree.jsx
@@ -1,20 +1,33 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import Grandpa from "./Grandpa";
 import "./FamilyTree.css";
 
 export const AssestContext = createContext("");
-const newAsset = "Gold";
+export const MoneyContext = createContext(0);
+
 const FamilyTree = () => {
+  const [money, setMoney] = useState(0);
+
   const asset = "diamond";
+  const newAsset = "Gold";
   return (
     <div className="family-tree">
       <h2>Family Tree</h2>
 
-      <AssestContext.Provider value={newAsset}>
+      {/* <AssestContext.Provider value={newAsset}>
         <Grandpa asset={asset} />
-      </AssestContext.Provider>
+      </AssestContext.Provider> */}
+      <h4>Total Family Money: {money}</h4>
+      <MoneyContext.Provider value={[money, setMoney]}>
+        <Grandpa></Grandpa>
+      </MoneyContext.Provider>
     </div>
   );
 };
 
 export default FamilyTree;
+
+/**
+ * 1. create a context using CreateContext with a default value
+ * 2. make sure you export the context to be used in other files
+ */
